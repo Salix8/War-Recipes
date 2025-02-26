e348b250d90b4c13db21570abe7e300b57aad030
SaulPachecoTrilles <78241212+Salix8@users.noreply.github.com>
2025-02-26 14:19:34 +0100

    He intentado mas animaciones no ha salido bn
    Idea tocar leyers
    o hacerlo manual

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -85,4 +85,73 @@ public class CharacterMovement : MonoBehaviour
         m_Rigidbody.MoveRotation(m_Rotation);
     }
 
+/*    using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public InputAction MoveAction;
+    public InputAction RunAction;
+
+    [SerializeField] private float walkSpeed = 3f;
+    [SerializeField] private float runSpeed = 6f;
+    [SerializeField] private float turnSpeed = 180f;
+
+    private Rigidbody m_Rigidbody;
+    private Animator m_Animator;
+    private AudioSource m_AudioSource;
+    private Vector3 m_Movement;
+    private Quaternion m_Rotation = Quaternion.identity;
+
+    private void Awake()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_Animator = GetComponent<Animator>();
+        m_AudioSource = GetComponent<AudioSource>();
+
+        MoveAction.Enable();
+        RunAction.Enable();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 pos = MoveAction.ReadValue<Vector2>();
+        bool isRunning = RunAction.IsPressed(); // Shift detectado
+
+        float horizontal = pos.x;
+        float vertical = pos.y;
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        float inputMagnitude = Mathf.Clamp01(m_Movement.magnitude);
+        
+        // Calcular velocidad basada en input
+        float targetSpeed = isRunning ? runSpeed : walkSpeed;
+        float speed = inputMagnitude * targetSpeed;
+
+        // Aplicar la velocidad al Blend Tree
+        m_Animator.SetFloat("Speed", speed / runSpeed); // Normalizar de 0 a 1
+
+        if (speed > 0)
+        {
+            m_Movement.Normalize(); // Normalizar después del cálculo de velocidad
+
+            // Mover al personaje
+            m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * speed * Time.deltaTime);
+
+            // Rotar en dirección del movimiento
+            Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Mathf.Deg2Rad * Time.deltaTime, 0f);
+            m_Rotation = Quaternion.LookRotation(desiredForward);
+            m_Rigidbody.MoveRotation(m_Rotation);
+
+            if (!m_AudioSource.isPlaying)
+            {
+                m_AudioSource.Play();
+            }
+        }
+        else
+        {
+            m_AudioSource.Stop();
+        }
+    }*/
+
 }
